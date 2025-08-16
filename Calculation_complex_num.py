a63890dafc752d6a855d507e7c5b2c9447ac9897
270648387 <270648387@yoobeestudent.ac.nz>
2025-08-16 18:18:57 +1200

    1

--- Calculation_complex_num.py
+++ Calculation_complex_num.py
@@ -1,20 +1,17 @@
-# Input 1
 real1 = float(input("Enter the real part of the first complex number: "))
 imag1 = float(input("Enter the imaginary part of the first complex number: "))
 c1 = complex(real1, imag1)
 
-# Input 2
+
 real2 = float(input("Enter the real part of the second complex number: "))
 imag2 = float(input("Enter the imaginary part of the second complex number: "))
 c2 = complex(real2, imag2)
 
-# Calculations
 sum_result = c1 + c2
 minus_result = c1 - c2
 product_result = c1 * c2
 divide_result = c1 / c2
 
-# Output the results
 print("Sum:", sum_result)
 print("Minus:", minus_result)
 print("Product:", product_result)
